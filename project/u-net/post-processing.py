0a5332a3d7e3bbdc93f504f4e6b2452342aa075d
Francesco Maglie <74652100+fra-mgl@users.noreply.github.com>
2025-05-16 11:40:36 +0200

    add area post processing, statistics

--- project/u-net/post-processing.py
+++ project/u-net/post-processing.py
@@ -45,15 +45,45 @@ csv_column_order = [
     "Passion au lait", "Arabia", "Stracciatella"
 ]
 
+area_threshold = {
+    1: 2224,
+    2: 2255,
+    3: 2231,
+    4: 2445,
+    5: 1151,
+    6: 1055,
+    7: 906,
+    8: 1389,
+    9: 1936,
+    10: 2234,
+    11: 1484,
+    12: 1735,
+    13: 1761
+}
+
+
+def count_instances(area, cls):
+    # print("Class: ", VOC_LABELS[cls])
+    # print("area: ", area)
+    # print("res: ", np.round(area / area_threshold[cls], 0))
+    return np.round(area / area_threshold[cls], 0)
+
 def post_processing(img_path, csv_writer, show):
     # mask = imread(os.path.join("./predict_output", img_path))
     img = Image.open(os.path.join("./predict_output", img_path)).convert('L')
     mask = np.array(img, dtype=np.uint8)
     
     binary_img = mask > 0
+    binary_img = opening(binary_img, disk(2))
     binary_img = remove_small_holes(binary_img) 
     binary_img = remove_small_objects(binary_img)
 
+    # binary_img = opening(mask, disk(2))
+    # binary_img = closing(mask, disk(1))
+    # binary_img = remove_small_holes(binary_img) 
+    # binary_img = remove_small_objects(binary_img)
+
+    
     # Label connected components
     label_image, label_num = measure.label(binary_img, return_num=True)
 
@@ -67,8 +97,9 @@ def post_processing(img_path, csv_writer, show):
             print(class_extract, int(filename.split('.')[0][1:]))
             print(np.unique(mask))
         else:
-            prediction_counts[class_extract] += 1        
+            prediction_counts[class_extract] += count_instances(np.count_nonzero(region), class_extract)
             improved_mask[label_image==i] = class_extract
+
 
 
     # append to csv
@@ -121,13 +152,22 @@ if __name__ == '__main__':
         writer = csv.writer(csv_file)
         writer.writerow(['id', 'Jelly White', 'Jelly Milk', 'Jelly Black', 'Amandina', 'Crème brulée', 'Triangolo', 'Tentation noir', 'Comtesse', 'Noblesse', 'Noir authentique', 'Passion au lait', 'Arabia', 'Stracciatella'])
 
-        # post_processing('L1000760.png', writer, show=True)
+        # post_processing('L1000944.png', writer, show=True)
+        # post_processing('L1010028.png', writer, show=True)
+        # post_processing('L1010001.png', writer, show=True)
+
         # exit(0)
 
+        i = 0
         with os.scandir(folder_path) as entries:
             for entry in entries:
                 if entry.is_file() and entry.name.lower().endswith('.png'):
                     filename = os.path.basename(entry.path)
                     post_processing(filename, writer, show=False)
+
+
+                    # i += 1
+                    # if i > 10:
+                    #     break
                 
                     
